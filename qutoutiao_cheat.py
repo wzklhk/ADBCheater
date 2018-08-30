77713a5a79c7b7e1f8cde50f20f87eb895a4db38
ZPAVelocity <velocity120@outlook.com>
2018-08-30 19:50:19 +0800

    make a class to adb cmd

--- qutoutiao_cheat.py
+++ qutoutiao_cheat.py
@@ -1,34 +1,10 @@
-import os
 import random
 import time
-
-DIFF = 10
-SCREEN_WIDTH = 1200
-SCREEN_LENGTH = 1920
-
-
-def swipe_screen(start_xy, end_xy):
-	""""""
-	cmd = "adb shell input swipe " \
-		+ str(start_xy[0]) + " " \
-		+ str(start_xy[1]) + " " \
-		+ str(end_xy[0]) + " " \
-		+ str(end_xy[1]) + " " 
-	print("cmd: ", cmd)
-	os.system(cmd)
-	
-	
-def tap_screen(xy):
-	""""""
-	cmd = "adb shell input tap " \
-		+ str(xy[0]) + " " \
-		+ str(xy[1]) + " " \
-		
-	print("cmd: ", cmd)
-	os.system(cmd)
+from screen_class import *
 
 	
 def main():
+	adb = AndroidDubugBrideCmd()
 	while True:
 		# fresh page
 		times = random.randint(2, 3)
@@ -37,7 +13,7 @@ def main():
 			FRESH_BUTTON_X = random.randint(0 + DIFF, SCREEN_WIDTH / 4 - DIFF)
 			FRESH_BUTTON_Y = random.randint(SCREEN_LENGTH - 120 + DIFF, SCREEN_LENGTH - DIFF)
 			FRESH_BUTTON = (FRESH_BUTTON_X, FRESH_BUTTON_Y)
-			tap_screen(FRESH_BUTTON)
+			adb.tap_screen(FRESH_BUTTON)
 			time.sleep(2)
 		
 		time.sleep(3)
@@ -46,7 +22,7 @@ def main():
 		TEXT_LAYER1_X = random.randint(SCREEN_WIDTH / 4 + DIFF, 3 * SCREEN_WIDTH / 4 - DIFF)
 		TEXT_LAYER1_Y = random.randint(250 + DIFF, 560 - DIFF)
 		TEXT_LAYER1 = (TEXT_LAYER1_X, TEXT_LAYER1_Y)
-		tap_screen(TEXT_LAYER1)
+		adb.tap_screen(TEXT_LAYER1)
 		time.sleep(5)
 
 		# swipe screen
@@ -60,14 +36,14 @@ def main():
 			SWIPE_END_AREA_X = random.randint(SCREEN_WIDTH * 3 / 8, SCREEN_WIDTH * 5 / 8)
 			SWIPE_END_AREA_Y = random.randint(SCREEN_LENGTH * 2 / 8, SCREEN_LENGTH * 3 / 8)
 			SWIPE_END_AREA = (SWIPE_END_AREA_X, SWIPE_END_AREA_Y)
-			swipe_screen(SWIPE_START_AREA, SWIPE_END_AREA)
+			adb.swipe_screen(SWIPE_START_AREA, SWIPE_END_AREA)
 			time.sleep(3)
 		
 		# back
 		BACK_BUTTON_X = random.randint(0 + DIFF, 100 - DIFF)
 		BACK_BUTTON_Y = random.randint(50 + DIFF, 150 - DIFF)
 		BACK_BUTTON = (BACK_BUTTON_X, BACK_BUTTON_Y )
-		tap_screen(BACK_BUTTON)
+		adb.tap_screen(BACK_BUTTON)
 		time.sleep(1)
 		
 
---
+++ screen_class.py
@@ -0,0 +1,54 @@
+import os
+
+SCREEN_WIDTH = 1200
+SCREEN_LENGTH = 1920
+DIFF = 10
+
+class AndroidDubugBrideCmd(object):
+    """Android Dubug Bride Cmd using in system"""
+    
+    def swipe_screen(self, start_xy, end_xy, press_time=None):
+        """swipe screen"""
+
+        # adb shell input swip x1 y1 x2 y2 press_time
+        if press_time == None:
+            cmd = "adb shell input swipe " \
+                + str(start_xy[0]) + " " \
+                + str(start_xy[1]) + " " \
+                + str(end_xy[0]) + " " \
+                + str(end_xy[1]) + " "
+        else:
+            cmd = "adb shell input swipe " \
+                + str(start_xy[0]) + " " \
+                + str(start_xy[1]) + " " \
+                + str(end_xy[0]) + " " \
+                + str(end_xy[1]) + " " \
+                + str(press_time)
+
+        print("cmd: ", cmd)
+        os.system(cmd)
+
+
+    def tap_screen(self, xy):
+        """tap screen"""
+
+        # adb shell input tap x y
+        cmd = "adb shell input tap " \
+            + str(xy[0]) + " " \
+            + str(xy[1]) + " " \
+            
+        print("cmd: ", cmd)
+        os.system(cmd)
+
+
+    def key_event(self, key):
+        """make key event"""
+
+        # adb shell input keyevent key
+        cmd = "adb shell input keyevent" \
+            + str(key)
+
+        print("cmd: ", cmd)
+        os.system(cmd)
+
+
