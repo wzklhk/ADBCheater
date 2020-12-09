1c5d54a4ee259b6c9c8c79841f379e4a33f96f17
zpavelocity <velocity120@outlook.com>
2020-12-10 00:32:26 +0800

    change readme

--- README.md
+++ README.md
@@ -1,18 +1,16 @@
-# QutoutiaoCheater
+# ADBCheater
 
-Read Qutoutiao text automatically
+Operating Android automatically
 
 ## 趣头条自动阅读器
 
 Just for fun
 
-使用ADB辅助和python脚本程序自动阅读趣头条的新闻
+使用ADB辅助和python脚本程序自动操作Android手机
 
 ## 原理
 
-1. 将手机打开到趣头条界面
-
-2. 用 ADB 工具点击屏幕和滑动屏幕
+用 ADB 工具点击屏幕和滑动屏幕
 
 adb shell input swipe x1 y1 x2 y2
 
@@ -22,6 +20,6 @@ adb shell input tap x y
 
 1. 正确配置ADB到系统环境变量
 
-2. 将screen_class.py文件中的SCREEN_WIDTH和SCREEN_LENGTH改为要使用手机的分辨率
+2. 将SCREEN_WIDTH和SCREEN_LENGTH改为要使用手机的分辨率
 
 3. 运行qutoutiao_cheat.py程序
--- qutoutiao_cheat.py
+++ qutoutiao_cheat.py
@@ -1,51 +1,54 @@
 import random
 import time
-from screen_class import *
+from ADBCmd import *
 
-	
+SCREEN_LENGTH = 1920
+SCREEN_WIDTH = 1080
+
+
 def main():
-	adb = AndroidDubugBrideCmd()
-	while True:
-		# fresh page
-		times = random.randint(2, 3)
-		print("fresh page: ", times, "times")
-		for i in range(times):
-			FRESH_BUTTON_X = random.randint(0 + DIFF, SCREEN_WIDTH / 4 - DIFF)
-			FRESH_BUTTON_Y = random.randint(SCREEN_LENGTH - 120 + DIFF, SCREEN_LENGTH - DIFF)
-			FRESH_BUTTON = (FRESH_BUTTON_X, FRESH_BUTTON_Y)
-			adb.tap_screen(FRESH_BUTTON)
-			time.sleep(2)
-		
-		time.sleep(3)
+    adb = ADBCmd()
+    while True:
+        # fresh page
+        times = random.randint(2, 3)
+        print("fresh page: ", times, "times")
+        for i in range(times):
+            FRESH_BUTTON_X = random.randint(0 + DIFF, SCREEN_WIDTH / 4 - DIFF)
+            FRESH_BUTTON_Y = random.randint(SCREEN_LENGTH - 120 + DIFF, SCREEN_LENGTH - DIFF)
+            FRESH_BUTTON = (FRESH_BUTTON_X, FRESH_BUTTON_Y)
+            adb.tap_screen(FRESH_BUTTON)
+            time.sleep(2)
 
-		# open text
-		TEXT_LAYER1_X = random.randint(SCREEN_WIDTH / 4 + DIFF, 3 * SCREEN_WIDTH / 4 - DIFF)
-		TEXT_LAYER1_Y = random.randint(250 + DIFF, 560 - DIFF)
-		TEXT_LAYER1 = (TEXT_LAYER1_X, TEXT_LAYER1_Y)
-		adb.tap_screen(TEXT_LAYER1)
-		time.sleep(5)
+        time.sleep(3)
 
-		# swipe screen
-		times = random.randint(8, 13)
-		print("swipe screen: ", times, "times")
-		for i in range(times):
-			print("time", i)
-			SWIPE_START_AREA_X = random.randint(SCREEN_WIDTH * 3 / 8, SCREEN_WIDTH * 5 / 8)
-			SWIPE_START_AREA_Y = random.randint(SCREEN_LENGTH * 5 / 8, SCREEN_LENGTH * 6 / 8)
-			SWIPE_START_AREA = (SWIPE_START_AREA_X, SWIPE_START_AREA_Y)
-			SWIPE_END_AREA_X = random.randint(SCREEN_WIDTH * 3 / 8, SCREEN_WIDTH * 5 / 8)
-			SWIPE_END_AREA_Y = random.randint(SCREEN_LENGTH * 2 / 8, SCREEN_LENGTH * 3 / 8)
-			SWIPE_END_AREA = (SWIPE_END_AREA_X, SWIPE_END_AREA_Y)
-			adb.swipe_screen(SWIPE_START_AREA, SWIPE_END_AREA)
-			time.sleep(3)
-		
-		# back
-		BACK_BUTTON_X = random.randint(0 + DIFF, 100 - DIFF)
-		BACK_BUTTON_Y = random.randint(50 + DIFF, 150 - DIFF)
-		BACK_BUTTON = (BACK_BUTTON_X, BACK_BUTTON_Y )
-		adb.tap_screen(BACK_BUTTON)
-		time.sleep(1)
-		
+        # open text
+        TEXT_LAYER1_X = random.randint(SCREEN_WIDTH / 4 + DIFF, 3 * SCREEN_WIDTH / 4 - DIFF)
+        TEXT_LAYER1_Y = random.randint(250 + DIFF, 560 - DIFF)
+        TEXT_LAYER1 = (TEXT_LAYER1_X, TEXT_LAYER1_Y)
+        adb.tap_screen(TEXT_LAYER1)
+        time.sleep(5)
+
+        # swipe screen
+        times = random.randint(8, 13)
+        print("swipe screen: ", times, "times")
+        for i in range(times):
+            print("time", i)
+            SWIPE_START_AREA_X = random.randint(SCREEN_WIDTH * 3 / 8, SCREEN_WIDTH * 5 / 8)
+            SWIPE_START_AREA_Y = random.randint(SCREEN_LENGTH * 5 / 8, SCREEN_LENGTH * 6 / 8)
+            SWIPE_START_AREA = (SWIPE_START_AREA_X, SWIPE_START_AREA_Y)
+            SWIPE_END_AREA_X = random.randint(SCREEN_WIDTH * 3 / 8, SCREEN_WIDTH * 5 / 8)
+            SWIPE_END_AREA_Y = random.randint(SCREEN_LENGTH * 2 / 8, SCREEN_LENGTH * 3 / 8)
+            SWIPE_END_AREA = (SWIPE_END_AREA_X, SWIPE_END_AREA_Y)
+            adb.swipe_screen(SWIPE_START_AREA, SWIPE_END_AREA)
+            time.sleep(3)
+
+        # back
+        BACK_BUTTON_X = random.randint(0 + DIFF, 100 - DIFF)
+        BACK_BUTTON_Y = random.randint(50 + DIFF, 150 - DIFF)
+        BACK_BUTTON = (BACK_BUTTON_X, BACK_BUTTON_Y)
+        adb.tap_screen(BACK_BUTTON)
+        time.sleep(1)
+
 
 if __name__ == "__main__":
-	main()
+    main()
